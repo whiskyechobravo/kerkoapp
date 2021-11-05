99a18d1675002d69dfc44497eca999c285a234ba
David Lesieur <david@davidlesieur.com>
2021-11-05 16:40:22 -0400

    Remove irrelevant pylint disables.

--- kerkoapp.py
+++ kerkoapp.py
@@ -3,10 +3,10 @@ from flask import redirect, url_for
 
 from app import create_app
 
-env = Env()  # pylint: disable=invalid-name
+env = Env()
 env.read_env()
 
-app = create_app(env.str('FLASK_ENV'))  # pylint: disable=invalid-name
+app = create_app(env.str('FLASK_ENV'))
 
 
 @app.route('/')
