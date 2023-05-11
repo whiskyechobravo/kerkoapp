b48e6755ca234c0123e456a223ae32d4a7d3c14f
David Lesieur <david@davidlesieur.com>
2023-05-11 19:10:51 -0400

    Adapt to Bootstrap-Flask changes.

--- kerkoapp/extensions.py
+++ kerkoapp/extensions.py
@@ -6,8 +6,8 @@ Each extension is initialized by the create_app factory of the app module.
 # pylint: disable=invalid-name
 
 from flask_babel import Babel, Domain
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap4
 
 babel_domain = Domain()
 babel = Babel(default_domain=babel_domain)
-bootstrap = Bootstrap()
+bootstrap = Bootstrap4()
