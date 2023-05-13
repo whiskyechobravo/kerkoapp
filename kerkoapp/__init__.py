1b10a403d0c0a211df3d2d9a090a18944d9ef752
David Lesieur <david@davidlesieur.com>
2023-05-12 20:12:42 -0400

    Fix Kerko translations not getting loaded with Flask-Babel 3.1.0.

--- kerkoapp/__init__.py
+++ kerkoapp/__init__.py
@@ -2,14 +2,15 @@
 A sample Flask application using the Kerko blueprint.
 """
 
-from flask import current_app, Flask, g, redirect, request, render_template
-from flask_babel import get_locale
+import pathlib
 
-from kerko import blueprint as kerko_blueprint
+import kerko
+from flask import Flask, render_template
+from flask_babel import Domain, get_locale
 
 from . import logging
 from .config import CONFIGS
-from .extensions import babel, babel_domain, bootstrap
+from .extensions import babel, bootstrap
 
 
 def create_app():
@@ -29,7 +30,15 @@ def create_app():
 
 
 def register_extensions(app):
-    babel.init_app(app)
+    # Configure Babel to use both Kerko's translations and the app's.
+    domain = ';'.join([kerko.TRANSLATION_DOMAIN, 'messages'])
+    translation_directories = ';'.join(kerko.TRANSLATION_DIRECTORIES + ['translations'])
+    babel.init_app(
+        app,
+        default_domain=domain,
+        default_translation_directories=translation_directories,
+    )
+
     logging.init_app(app)
     bootstrap.init_app(app)
 
@@ -37,14 +46,13 @@ def register_extensions(app):
 def register_blueprints(app):
     # Setting `url_prefix` is required to distinguish the blueprint's static
     # folder route URL from the app's.
-    app.register_blueprint(kerko_blueprint, url_prefix='/bibliography')
+    app.register_blueprint(kerko.blueprint, url_prefix='/bibliography')
 
 
 def register_errorhandlers(app):
     def render_error(error):
         # If a HTTPException, pull the `code` attribute; default to 500.
         error_code = getattr(error, 'code', 500)
-        babel_domain.as_default()
         context = {
             'locale': get_locale(),
         }
--- kerkoapp/extensions.py
+++ kerkoapp/extensions.py
@@ -3,11 +3,9 @@ Instantiate extensions.
 
 Each extension is initialized by the create_app factory of the app module.
 """
-# pylint: disable=invalid-name
 
-from flask_babel import Babel, Domain
+from flask_babel import Babel
 from flask_bootstrap import Bootstrap4
 
-babel_domain = Domain()
-babel = Babel(default_domain=babel_domain)
+babel = Babel()
 bootstrap = Bootstrap4()
