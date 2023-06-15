e86c6a05e665aa79cbb0de0c0d383249971bb6f8
David Lesieur <david@davidlesieur.com>
2023-06-15 18:50:54 -0400

    Validate mandatory variables at config root.

--- kerkoapp/__init__.py
+++ kerkoapp/__init__.py
@@ -8,7 +8,8 @@ import pathlib
 import kerko
 from flask import Flask, render_template
 from flask_babel import get_locale
-from kerko.config_helpers import config_update, load_toml, parse_config
+from kerko.config_helpers import (KerkoModel, RootModel, config_update,
+                                  load_toml, parse_config)
 
 from . import logging
 from .config_helpers import KerkoAppModel, load_config_files
@@ -35,7 +36,8 @@ def create_app() -> Flask:
     app.config.from_prefixed_env(prefix='KERKOAPP')
 
     # Validate configuration and save its parsed version.
-    parse_config(app.config, 'kerko')
+    parse_config(app.config, None, RootModel)
+    parse_config(app.config, 'kerko', KerkoModel)
     parse_config(app.config, 'kerkoapp', KerkoAppModel)
 
     # Initialize the Composer object.
